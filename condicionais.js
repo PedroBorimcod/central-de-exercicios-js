1649e047175bcfcdbb0a6c4487e4778df5cc9e2c
Pedro Borim <pepelupilipe@gmail.com>
2025-04-03 21:52:15 -0300

    Inicio js

---
+++ condicionais.js
@@ -0,0 +1,54 @@
+// let numero = prompt("Digite um numero:");
+
+// if (numero > 0) {
+
+//     console.log("Positivo");
+
+// } else {
+//     if (numero <0)  {
+
+//         console.log ("Negativo");
+    
+//     }else {
+
+//         console.log("É zero.")
+
+//     }   
+
+
+
+// }
+
+
+// let numero = prompt ("Insira sua idade:")
+
+// if (numero > 18) {
+
+//     console.log("É maior de idade")
+
+// }else{
+
+//     if(numero < 18){
+//         console.log("É menor de idade")
+//     }else{
+
+//         console.log("É maior de idade")
+
+//     }
+
+// }
+
+
+
+
+let numero= prompt("Divida o numero")
+
+if(numero % 2 === 0){
+    console.log(`O numero ${numero} é divisiel`)
+}else{
+    console.log(`O numero ${numero} nao e divisivel`)
+}
+
+
+
+
--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-alert("Hello Word na tela!");
+// alert("Hello Word na tela!");
 
-console.log("Hello Word!")
+// console.log("Hello Word!");
+
+
+let nome="Pedro Borim";
+const idade= 18;
+var cidade="sao paulo";
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+let texto="Meu texto";
+let numero= 20;
+
+
+console.log("Meu nome é: "+ nome);  //* concatenaçao
+console.log(`Meu nome é: ${nome}`)  //*interpolacao
+console.log(`Tenho: ${idade} anos de idade `)
+console.log(`Minha cidade é: ${cidade}`)
+
+
+
+console.log(typeof idade);
+
+let n1 ="10";
+let n2 =10;
+let soma = Number (n1) + n2 ;
+console.log(soma);
+
+
+
+
+
