94a329976e5f59acff9780dcecda4d8214c3115b
Pedro Borim <pepelupilipe@gmail.com>
2025-04-09 21:55:13 -0300

    inicio ulteis.js

---
+++ uteis.js
@@ -0,0 +1,10 @@
+function exemplodefilter(){
+
+    let numeros =[1, 2, 3, 4, 5 ,6 ,7 ,8 ,9 ,10];
+
+    let numerosMaiorQueDois = numeros.filter(numero => numero > 2);
+    console.log(numerosMaiorQueDois);
+
+
+}
+exemplodefilter()
